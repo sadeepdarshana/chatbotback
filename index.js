075065a6240f7f55851e529753ed8248a05662c9
sadeep <darshanasadeep@gmail.com>
2019-07-15 23:56:36 +0530

    port

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express()
 const port = process.env.PORT || 8000
 
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
 
 const weatherApiLibrary = require('apixu');
 const config = weatherApiLibrary.config;
@@ -23,7 +30,7 @@ function prepareCurrentResponse(rawWeatherData){
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/*', (req, res) => res.send(req.body))
 
 
 
